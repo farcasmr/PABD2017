dca2da027e73353c03ec1a062bdab0898080ffa0
ChicideaD <danyy_adc@yahoo.com>
2017-06-02 20:23:12 +0300

    Pagina personala
    Am incarcat doua fisier cu pagina personala

---
+++ dan_pagina1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<style>
+next {
+    left: 129px;
+    width: 43px;
+    background: url('img.png') -91px 0;
+}
+</style>
+<body>
+
+
+"Prima Mea Pagina!"
+ <br>
+<next id="next"><a href="dan_pagina.php"> << </a></li>
+ 
+</body>
+
+</html>
