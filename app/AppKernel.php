7e39e46e0661b68de4cf158acc3706e3e7a0de72
Tecsa Roxana <tecsaroxana30@gmail.com>
2017-06-03 11:09:05 +0300

    rezolvare conflict

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,10 +20,10 @@ class AppKernel extends Kernel
             new Uab\FlorinBundle\UabFlorinBundle(),
             new Uab\MagerusanBundle\UabMagerusanBundle(),
             new Uab\SolymosiBundle\UabSolymosiBundle(),
-          	new Uab\BreazBundle\UabBreazBundle(),
-            
+            new Uab\BreazBundle\UabBreazBundle(),
 	    new Uab\PocrisBundle\UabPocrisBundle(),
-            new Uab\TecsaBundle\UabTecsaBundle(),
+	    new Uab\IspasBundle\UabIspasBundle(),
+            new Uab\TecsaBundle\UabTecsaBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
